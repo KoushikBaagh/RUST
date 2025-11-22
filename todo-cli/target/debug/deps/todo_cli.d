4816cac6434ad214a0e08d90428249ada6eb1e71
KoushikBaagh <koushikbug123@gmail.com>
2025-11-22 17:45:53 +0530

    Refactor code structure for improved readability and maintainability

---
+++ todo-cli/target/debug/deps/todo_cli.d
@@ -0,0 +1,5 @@
+C:\VSCode\RUST\todo-cli\target\debug\deps\todo_cli.d: src\main.rs
+
+C:\VSCode\RUST\todo-cli\target\debug\deps\todo_cli.exe: src\main.rs
+
+src\main.rs:
---
+++ todo-cli/target/debug/todo-cli.d
@@ -0,0 +1,1 @@
+C:\VSCode\RUST\todo-cli\target\debug\todo-cli.exe: C:\VSCode\RUST\todo-cli\src\main.rs
