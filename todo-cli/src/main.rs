64a1a570b3f7f4dd1a38d14bb2dcb9543a993b87
KoushikBaagh <koushikbug123@gmail.com>
2025-11-23 10:19:27 +0530

    fix: rust-analyzer issue

--- todo-cli/src/main.rs
+++ todo-cli/src/main.rs
@@ -1,5 +1,13 @@
 use std::io :: {self, BufRead};
 
 fn main() {
-    println()
+    println("Welcome my App")
+    let stdin = stdin.lock().lines();
+    let mut lines = stdin.lock().lines();
+
+    println!("Type something and press Enter");
+    if let Some(Ok(line)) = lines.next()
+    {
+        println("You typed : {} ", line);
+    }
 }
